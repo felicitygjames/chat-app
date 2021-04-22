cc817f483c7ec02e2f23bcd7bdbe516993c105df
felicitygjames <72702947+felicitygjames@users.noreply.github.com>
2021-04-22 18:46:54 -0500

    added all files

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  Alert,
+  ScrollView,
+} from "react-native";
+import Start from "./components/Start";
+import Chat from "./components/Chat";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator();
+
+export default class HelloWorld extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+  }
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Start">
+          <Stack.Screen name="Start" component={Start} />
+          <Stack.Screen name="Chat" component={Chat} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  box1: {
+    flex: 1,
+    backgroundColor: "red",
+    justifyContent: "center",
+  },
+  box2: {
+    flex: 1,
+    backgroundColor: "green",
+    justifyContent: "center",
+  },
+  box3: {
+    flex: 1,
+    backgroundColor: "yellow",
+    justifyContent: "center",
   },
 });
